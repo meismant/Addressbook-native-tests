2d4b589ea0623da03f334848ad98943721a6dea8
maskaradm <maskaradm@gmail.com>
2015-08-05 10:02:11 +0300

    Task 18

---
+++ src/com/example/fw/ContactHelper.java
@@ -0,0 +1,44 @@
+package com.example.fw;
+
+public class ContactHelper extends HelperBase {
+
+	public ContactHelper(ApplicationManager applicationManager) {
+		super(applicationManager);
+	}
+
+	public void createContact(Contact contact) {
+		initContactCreation();
+		fillContactForm(contact);
+		confirmContactCreation();
+	}
+
+	public void initContactCreation() {
+		manager.getAutoItHelper()
+				.winWaitAndActivate("AddressBook Portable", "", 5000)
+				.click("Add").winWaitAndActivate("Add Contact", "", 5000);
+	}
+
+	public void fillContactForm(Contact contact) {
+		manager.getAutoItHelper().send("TDBEdit12", contact.getFirstName())
+				.send("TDBEdit11", contact.getLastName());
+	}
+
+	public void confirmContactCreation() {
+		manager.getAutoItHelper().click("Save")
+				.winWaitAndActivate("AddressBook Portable", "", 5000);
+	}
+
+	public Contact getFirstContact() {
+		manager.getAutoItHelper()
+				.winWaitAndActivate("AddressBook Portable", "", 5000)
+				.focus("TListView1").send("{DOWN}{SPACE}").click("Edit")
+				.winWaitAndActivate("Update Contact", "", 5000);
+		Contact contact = new Contact().setFirstName(
+				manager.getAutoItHelper().getText("TDBEdit12")).setLastName(
+				manager.getAutoItHelper().getText("TDBEdit11"));
+		manager.getAutoItHelper().click("Cancel")
+				.winWaitAndActivate("AddressBook Portable", "", 5000);
+		return contact;
+
+	}
+}
---
+++ src/com/example/tests/TestBase.java
@@ -0,0 +1,44 @@
+package com.example.tests;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+import com.example.fw.ApplicationManager;
+
+public class TestBase {
+	protected Logger log = Logger.getLogger("TEST");
+	protected ApplicationManager app;
+
+	@BeforeClass
+	@Parameters({ "configFile" })
+	public void setUp(@Optional String configFile) throws Exception {
+		if (configFile == null) {
+			configFile = System.getProperty("configFile");
+		}
+		if (configFile == null) {
+			configFile = System.getenv("configFile");
+		}
+		if (configFile == null) {
+			configFile = "application.properties";
+		}
+		Properties properties = new Properties();
+		properties.load(new FileReader(new File(configFile)));
+		log.info("setUp start");
+		app = ApplicationManager.getInstance(properties);
+		log.info("setUp end");
+	}
+
+	@AfterTest
+	public void tearDown() throws Exception {
+		log.info("tearDown start");
+		ApplicationManager.getInstance(null).stop();
+		log.info("tearDown end");
+	}
+}
